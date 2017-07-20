994e411c7709ec1044e9aa4e1b8cdbab981bb5a9
chiou <bohemianwind@gmail.com>
2017-07-20 09:33:33 +0800

    Some experiments and exercises for MapKit

--- TestDogsMap/TestDogsMap/PickPlaceVC.swift
+++ TestDogsMap/TestDogsMap/PickPlaceVC.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class PickPlaceVC: UIViewController, MKMapViewDelegate {
+class PickPlaceVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    var lmgr:CLLocationManager?
+    
     @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var mapView: MKMapView!
     
@@ -19,11 +22,52 @@ class PickPlaceVC: UIViewController, MKMapViewDelegate {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func showUser(_ sender: Any) {
+        let userLoc = lmgr?.location
+        
+        if !mapView.showsUserLocation {
+            mapView.showsUserLocation = true
+            print("User location: \(userLoc?.coordinate.latitude), \(userLoc?.coordinate.longitude)")
+        } else {
+            mapView.showsUserLocation = false
+        }
+    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("load pickPlaceVC")
-
+    @IBAction func allowOnlyOne(_ sender: Any) {
+    }
+    
+    @IBAction func allowMultipleOnes(_ sender: Any) {
+    }
+    
+    @IBAction func cleanAnnotations(_ sender: Any) {
+        let allAnnotations = mapView.annotations
+        mapView.removeAnnotations(allAnnotations)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations.first as! CLLocation
+        
+        print("locationManager: \(userLocation.coordinate.latitude), \(userLocation.coordinate.longitude)")
+        
+    }
+    
+    
+    private func initStat() {
+        
+        
+        if CLLocationManager.locationServicesEnabled() {
+            
+            lmgr = CLLocationManager()
+            
+            lmgr?.delegate = self
+            lmgr?.desiredAccuracy = kCLLocationAccuracyBest
+            lmgr?.requestWhenInUseAuthorization()
+            lmgr?.startUpdatingLocation()
+            lmgr?.distanceFilter = CLLocationDistance(10)
+        }
+    }
+    
+    private func initStatMap() {
         mapView.delegate = self
         
         let mapSingleTapReg = UITapGestureRecognizer(target: self, action: #selector(mapTapHandle) )
@@ -39,10 +83,19 @@ class PickPlaceVC: UIViewController, MKMapViewDelegate {
         
         // to distinguish single- and double-tap
         mapSingleTapReg.require(toFail: mapDoubleTapReg )
-
+        
         // to make mapView recognizing single- and double-tap gestures
         mapView.addGestureRecognizer(mapSingleTapReg)
         mapView.addGestureRecognizer(mapDoubleTapReg)
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("load pickPlaceVC")
+        
+        initStat()
+        initStatMap()
 
     }
 
